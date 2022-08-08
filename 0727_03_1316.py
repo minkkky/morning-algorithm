704282b572042877dc494f68f9820e5bfb609f46
m_ink <mp98dl4@gmail.com>
2022-08-08 15:03:40 +0900

    0808 제출

--- 0727_03_1316.py
+++ 0727_03_1316.py
@@ -1,2 +1,19 @@
 # 그룹 단어 체커
 # https://www.acmicpc.net/problem/1316
+
+n = int(input())
+cnt = n
+
+for i in range(n):
+    word = input()
+    for j in range(0, len(word)-1):
+        # print(word[j])
+        # print(word[j+1])
+        # print(word[j+1:])
+        if word[j] ==  word[j+1]:
+            pass
+        elif word[j] in word[j+1:]:
+            cnt -= 1
+            break
+
+print(cnt)
--- 0728_02_1011.py
+++ 0728_02_1011.py
@@ -1,10 +1,23 @@
 # Fly me to the Alpha Centauri
 # https://www.acmicpc.net/problem/1011
 
+# 절반으로 나눠서 앞은 커지고 뒤는 작아지는 로직이어야 하나...?
+# print(m/2, m//2, m%2)
+# 참고: https://ooyoung.tistory.com/91
+
 loop_cnt = int(input())
-cnt = 0
 
 for i in range(loop_cnt):
     x, y = map(int, input().split(' '))
-    m = y-x # 이동거리
-    # 절반으로 나눠서 앞은 커지고 뒤는 작아지는 로직이어야 하나...?
+    d = y-x # 이동거리
+    cnt = 0 # 이동회수
+    mp = 1 # 이동가능거리
+    mp_plus = 0 # 이동한 거리의 합
+    
+    while mp_plus < d :
+        cnt += 1
+        mp_plus += mp
+        if cnt % 2 == 0:
+            mp += 1
+
+    print(cnt)
