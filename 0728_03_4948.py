5c4e84ac111191e2b8c9a995715c498e92a01f9f
m_ink <mp98dl4@gmail.com>
2022-08-17 10:17:32 +0900

    0817 제출

--- 0728_03_4948.py
+++ 0728_03_4948.py
@@ -1,2 +1,32 @@
 # 베르트랑 공준
 # https://www.acmicpc.net/problem/4948
+
+import math
+
+n = 246912
+array = [True for i in range(n + 1)]
+primary_num = []
+
+for i in range(2, int(math.sqrt(n)) + 1):
+    if array[i] == True:
+        j = 2
+        while i * j <= n:
+            array[i * j] = False
+            j += 1
+
+for i in range(2, n + 1):
+    if array[i]:
+        primary_num.append(i)
+
+while n != 0:
+    n = int(input())
+    res = 0
+
+    if n == 0:
+        break
+
+    for i in primary_num:
+        if n < i <= 2*n:
+            res += 1
+
+    print(res)
--- 0801_03_1929.py
+++ 0801_03_1929.py
@@ -1,1 +1,20 @@
 # https://www.acmicpc.net/problem/1929
+
+# 에라토스테네스의 체 알고리즘
+# https://coding-of-today.tistory.com/170?category=995820
+import math
+
+n = 1000000
+array = [True for i in range(n + 1)]
+
+for i in range(2, int(math.sqrt(n)) + 1):
+    if array[i] == True:
+        j = 2
+        while i * j <= n:
+            array[i * j] = False
+            j += 1
+
+x,y = map(int, input().split())
+for i in range(x, y+ 1):
+    if array[i]:
+        print(i)
