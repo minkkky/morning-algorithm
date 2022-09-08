56a6c957e86b18f6ab75168144702f921387c4cf
m_ink <mp98dl4@gmail.com>
2022-09-08 15:43:08 +0900

    220908 알고리즘 풀이

--- 0802_02_ 11651.py
+++ 0802_02_ 11651.py
@@ -1,1 +1,13 @@
 # https://www.acmicpc.net/problem/11651
+
+n = int(input())
+dots = []
+
+for i in range(n):
+    a, b = map(int, input().split())
+    dots.append((a, b))
+
+sorted_dots = sorted(dots, key=lambda x: (x[1], x[0]))
+
+for i in range(n):
+    print(f'{sorted_dots[i][0]} {sorted_dots[i][1]}')
